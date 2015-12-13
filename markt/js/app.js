661f089883c13e168f363078f1f6afc1801c03fa
Mart van de Ven <m@type.hk>
2015-12-14 02:46:57 +0800

    Market, Predict, Portfolio Views

--- markt/js/app.js
+++ markt/js/app.js
@@ -3,6 +3,10 @@ $(document).foundation();
 var APPID = 'shining-fire-9795'
 var ref = new Firebase("https://" + APPID + ".firebaseio.com/");
 
+$(".button-modal").animatedModal({
+	"animatedIn" : 'fadeInDownBig',
+	"animatedOut" : 'fadeOutUp',
+	});
 
 // Handle Logins
 function authHandler(error, authData) {

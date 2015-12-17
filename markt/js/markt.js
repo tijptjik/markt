51249501148c504b954283d2a999420675228f21
Mart van de Ven <m@type.hk>
2015-12-17 12:35:25 +0800

    split up js

---
+++ markt/js/markt.js
@@ -0,0 +1,18 @@
+
+// Modals
+
+$(".button-modal").animatedModal({
+	"animatedIn" : 'fadeInDownBig',
+	"animatedOut" : 'fadeOutUp',
+});
+
+// Firebase
+
+var APPID = 'shining-fire-9795'
+var ref = new Firebase("https://" + APPID + ".firebaseio.com/");
+
+/* Post New Prediction */
+
+function postPrediction(){
+
+}

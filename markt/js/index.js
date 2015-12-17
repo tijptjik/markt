2ab468e600e44b200438a0050a07c5e19e6a4027
Mart van de Ven <m@type.hk>
2015-12-17 14:53:51 +0800

    Post Predictions

--- markt/js/index.js
+++ markt/js/index.js
@@ -4,63 +4,38 @@
 var APPID = 'shining-fire-9795'
 var ref = new Firebase("https://" + APPID + ".firebaseio.com/");
 
-/* Login */
 
-// Handle Logins
-function authHandler(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-    // Redirect to 1st App View
-    window.location.href = "./dashboard.html";
-  }
-}
+/* Event Listeners */
 
-// Create a callback which logs the current auth state
-function authDataCallback(authData) {
-  if (authData) {
-    console.log("User " + authData.uid + " is logged in with " + authData.provider);
-  } else {
-    console.log("User is logged out");
-  }
-}
-
-function loadApp(){
-	$main = $('main')
-	$main.removeClass('align-middle');
-	$main.find('.log-in-form').remove();
-	$main.find('#markt').toggle();
-}
-
-// Trigger Logins
-function loginFlash(callout, formData, authHandler){
-	callout.fadeIn().removeClass('alert').addClass('success');
-  	callout.find('h5').text('Successfully opened account')
-  	callout.find('.msg').text("Logging you in ...")
-    ref.authWithPassword(formData, authHandler);
-}
+// LOGGING IN
 
 $('.log-in-form form .submit').on('click', function(e){
 	e.preventDefault();
+	submitHandler(this);	
+});
 
+// Upon Authentication
+
+ref.onAuth(authDataCallback);
+	
+
+/* EVENT HANDLERS  */
+
+// Form Submit
+
+function submitHandler(that){
 	var $callout = $('.callout');
 	$callout.fadeOut();
-	
-	var formData = $(this).parent('form').serializeArray()
-		.reduce(function(obj, item) {
-		    obj[item.name] = item.value;
-		    return obj;
-		},{});
 
+	post = formData(that);
 	// Create Accounts
-	ref.createUser(formData, function(error, userData) {
+	ref.createUser(post, function(error, userData) {
 
 	  if (error) {	
 	    switch (error.code) {
 	      case "EMAIL_TAKEN":
 	      	// Login With an email/password combination
-			loginFlash($callout,formData,authHandler);
+			loginFlash($callout, post, authHandler);
 	        break;
 	      case "INVALID_EMAIL":
 	      	$callout.fadeIn();
@@ -72,11 +47,68 @@ $('.log-in-form form .submit').on('click', function(e){
 	    }
 	  } else {
 	  	// Login With an email/password combination
-	  	loginFlash($callout,formData,authHandler);
+	  	loginFlash($callout, post, authHandler);
 	  }
 	});
-});
+};
 
-// Check if login is persisted.
-ref.onAuth(authDataCallback);
+
+// Logging in
+
+function loginFlash(callout, formData, authHandler){
+	callout.fadeIn().removeClass('alert').addClass('success');
+  	callout.find('h5').text('Successfully opened account')
+  	callout.find('.msg').text("Logging you in ...")
+    ref.authWithPassword(formData, authHandler);
+}
+
+function authHandler(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+}
+
+
+// Create a callback which logs the current auth state
+function authDataCallback(authData) {
+  if (authData) {
+    // save the user's profile into the database so we can list users,
+    // use them in Security and Firebase Rules, and show profiles
+    ref.child("users").child(authData.uid).set({
+      provider: authData.provider,
+      name: getName(authData)
+    });
+
+    // Redirect to 1st App View
+    window.location.href = "./dashboard.html";
+  };
+};
+
+/* UTILITY FUNCTIONS */
+
+// find a suitable name based on the meta info given by each provider
+function getName(authData) {
+  switch(authData.provider) {
+     case 'password':
+       return authData.password.email.replace(/@.*/, '');
+     case 'twitter':
+       return authData.twitter.displayName;
+     case 'facebook':
+       return authData.facebook.displayName;
+  }
+}
+
+
+// Gather the form data
+function formData(that){
 	
+	var formData = $(that).parent('form').serializeArray()
+		.reduce(function(obj, item) {
+		    obj[item.name] = item.value;
+		    return obj;
+		},{});
+	
+	return formData;
+}
--- markt/js/markt.js
+++ markt/js/markt.js
@@ -1,18 +1,108 @@
 
-// Modals
-
-$(".button-modal").animatedModal({
-	"animatedIn" : 'fadeInDownBig',
-	"animatedOut" : 'fadeOutUp',
-});
-
 // Firebase
 
 var APPID = 'shining-fire-9795'
 var ref = new Firebase("https://" + APPID + ".firebaseio.com/");
 
+/* Set Timestamps */
+
+function timestamp(obj){
+	if (!obj.hasOwnProperty('created_at')){
+		obj['created_at'] = Firebase.ServerValue.TIMESTAMP;
+	}
+
+	obj['updated_at'] = Firebase.ServerValue.TIMESTAMP;
+	return obj;
+}
+
 /* Post New Prediction */
 
-function postPrediction(){
+function withUserName(e, callback){
+	getUsername({}, e, callback);
+}
+
+function postPrediction(options, e, callback){
+
+	var $form = $(e.target).parents('.predict');
+	var pred = {};
+	var bet = {};
+
+	pred['prediction'] = $form.find('textarea[name=prediction]').val();
+	pred['condition'] = $form.find('textarea[name=condition]').val();
+	pred['user_id'] = options.user.uid;
+	pred['user_name'] = options.user.name;
+	pred = timestamp(pred)
+	
+	var newPredictionRef = ref.child("predictions").push(pred);
+	var predictionID = newPredictionRef.key();
+	
+	bet['amount'] = parseFloat($form.find('.slider-handle').attr('aria-valuenow'));
+	bet['yes'] = true;
+	bet['prediction'] = predictionID;
+	bet['user_id'] = options.user.uid;
+	bet['user_name'] = options.user.name;
+	bet = timestamp(bet);
+
+	var newBetRef = ref.child("bets").push(bet);
+
+	var betID = newBetRef.key();
+
+	ref.child('users/' + options.user.uid + '/bets/' + betID).set(bet);
+
+	callback();
+}
+
+
+/* Event Listeners */
+
+$(function(){
+
+	// Post New Prediction
+	$('.predict .button.submit').on('click', function(e){
+		e.preventDefault();
+
+		withUserName(e, function(options){
+			postPrediction(options, e,
+				function(){
+					window.location.href = "./dashboard.html";
+			});
+		});
+	});
+
+
+	// Logging out.
+	$('.button.logout').on('click', logOut);
+
+});
+
+/* Event Handlers */
+
+function logOut(e){
+	e.preventDefault();
+	ref.unauth();
+	window.location.href = './'
+}
+
+
+// UTILITY FUNCTIONS
+
+function getUserID(){
+	authData = ref.getAuth();
+	return authData.uid;
+}
+
+function getUsername(obj, e, callback){
+	authData = ref.getAuth();
+	ref.child('users/' + authData.uid + '/name').once("value", function(snapshot) {
+		  var user = {}
+		  user['name'] = snapshot.val()
+		  user['uid'] = authData.uid 
+		  
+		  obj['user'] = user;
+		  callback(obj);
+
+		}, function (errorObject) {
+		  console.log("The read failed: " + errorObject.code);
+		});
 
 }
